eb4c8ad67e393a996b47812be2bb2ba71ba61bb2
Simon Ansell <siansell@gmail.com>
2021-02-17 17:00:11 +0000

    support multiple boards

--- src/board.js
+++ src/board.js
@@ -1,12 +1,20 @@
+import { v4 as uuid } from 'uuid'
 import * as d3 from 'd3'
 
+// @TODO: orientation 'w' | 'b'
+// @TODO: showNotation just show rank and file on edge of board
+// @TODO: callbacks onSquareClick etc.
 const defaultConfig = {
   boardDimension: 8,
+  blackSquareColour: '#dee3e0',
+  borderColour: '#f0f5f2',
+  borderStyle: 'solid',
+  borderWidth: 10,
   marginLeft: 30,
   marginTop: 30,
+  showNotation: true,
   squareSize: 80,
   whiteSquareColour: '#f0f5f2',
-  blackSquareColour: '#dee3e0',
 }
 
 const getFile = (x) => String.fromCharCode(x + 97) // Maps 0 -> 'a', 1 -> 'b' etc.
@@ -15,23 +23,33 @@ const getSquareName = (d, boardDimension) => `${getFile(d.x)}${getRank(d.y, boar
 const isWhiteSquare = (x, y) => (x % 2 === 0 && y % 2 === 0) || (x % 2 === 1 && y % 2 === 1)
 
 const drawBoard = (rootSelector = 'body', config = defaultConfig) => {
-  const mergedConfig = Object.assign(defaultConfig, config)
+  const mergedConfig = { ...defaultConfig, ...config }
   const {
-    marginTop, marginLeft, squareSize, boardDimension, whiteSquareColour, blackSquareColour,
+    blackSquareColour,
+    borderColour,
+    boardDimension,
+    borderStyle,
+    borderWidth,
+    marginLeft,
+    marginTop,
+    showNotation,
+    squareSize,
+    whiteSquareColour,
   } = mergedConfig
   const boardSize = boardDimension * squareSize
-  const border = `10px solid ${whiteSquareColour}`
+  const border = borderStyle === 'none' ? 'none' : `${borderWidth}px ${borderStyle} ${borderColour}`
 
-  const board = []
+  const squares = []
+  const boardId = `board-${uuid()}`
 
   for (let i = 0; i < boardDimension * boardDimension; i++) {
-    board.push({
+    squares.push({
       x: i % boardDimension,
       y: Math.floor(i / boardDimension),
     })
   }
 
-  const div = d3
+  const board = d3
     .select(rootSelector)
     .append('div')
     .style('margin-top', `${marginTop}px`)
@@ -39,13 +57,14 @@ const drawBoard = (rootSelector = 'body', config = defaultConfig) => {
     .style('width', `${boardSize}px`)
     .style('height', `${boardSize}px`)
     .style('border', border)
+    .attr('id', boardId)
 
-  const svg = div
+  const svg = board
     .append('svg')
     .attr('width', `${boardSize}px`)
     .attr('height', `${boardSize}px`)
     .selectAll()
-    .data(board)
+    .data(squares)
     .enter()
     .append('g')
 
@@ -56,17 +75,25 @@ const drawBoard = (rootSelector = 'body', config = defaultConfig) => {
     .attr('width', `${squareSize}px`)
     .attr('height', `${squareSize}px`)
     .style('fill', (d) => (isWhiteSquare(d.x, d.y) ? whiteSquareColour : blackSquareColour))
+    // @TODO onSquareClick callback
     .on('click', (e, d) => console.log(e.target, d, getSquareName(d, boardDimension)))
 
-  svg
-    .append('text')
-    .attr('x', (d) => d.x * squareSize + squareSize / 2)
-    .attr('y', (d) => d.y * squareSize + squareSize / 2)
-    .attr('text-anchor', 'middle')
-    .attr('dominant-baseline', 'middle')
-    .style('font-family', 'Verdana')
-    .style('pointer-events', 'none')
-    .text((d) => getSquareName(d, boardDimension))
+  if (showNotation) {
+    svg
+      .append('text')
+      .attr('x', (d) => d.x * squareSize + squareSize / 2)
+      .attr('y', (d) => d.y * squareSize + squareSize / 2)
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'middle')
+      .style('font-family', 'Verdana')
+      .style('pointer-events', 'none')
+      .text((d) => getSquareName(d, boardDimension))
+  }
+
+  return {
+    boardId,
+    board,
+  }
 }
 
 export default drawBoard
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import drawBoard from './board'
 import pgnParser from './lib/pgn-parser'
 import pgnData from './games'
 
-drawBoard('#board1')
-drawBoard('#board2', { boardDimension: 6 })
+const board1 = drawBoard('#board1', { borderStyle: 'dashed', borderWidth: 4, whiteSquareColour: 'whitesmoke' })
+const board2 = drawBoard('#board2', {
+  boardDimension: 6, marginLeft: 100, squareSize: 40, showNotation: false,
+})
+console.log(board1, board2)
 
 const result = pgnParser.parse(pgnData)
 console.log(result)
