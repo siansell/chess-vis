b5600282e776bdf87b6b588c0626c7c7dbcd1dbb
Simon Ansell <siansell@gmail.com>
2021-02-17 15:19:56 +0000

    Add eslint

--- index.js
+++ index.js
@@ -6,60 +6,56 @@ const squareSize = 80
 const boardDimension = 8
 const boardSize = boardDimension * squareSize
 
-const whiteSquareColour = "#f0f5f2"
-const blackSquareColour = "#dee3e0"
+const whiteSquareColour = '#f0f5f2'
+const blackSquareColour = '#dee3e0'
 
-const getFile = x => String.fromCharCode(x + 97) // Maps 0 -> 'a', 1 -> 'b' etc.
-const getRank = y => boardDimension - y % boardDimension
-const getSquareName = d => `${getFile(d.x)}${getRank(d.y)}`
+const getFile = (x) => String.fromCharCode(x + 97) // Maps 0 -> 'a', 1 -> 'b' etc.
+const getRank = (y) => boardDimension - (y % boardDimension)
+const getSquareName = (d) => `${getFile(d.x)}${getRank(d.y)}`
 const isWhiteSquare = (x, y) => (x % 2 === 0 && y % 2 === 0) || (x % 2 === 1 && y % 2 === 1)
 
-const board = [];
+const board = []
 
 for (let i = 0; i < boardDimension * boardDimension; i++) {
-    board.push({
-        x: i % boardDimension,
-        y: Math.floor(i / boardDimension),
-    })
+  board.push({
+    x: i % boardDimension,
+    y: Math.floor(i / boardDimension),
+  })
 }
 
 const div = d3
-    .select("body")
-    .append("div")
-    .style("position", "fixed")
-    .style("top", `${marginTop}px`)
-    .style("left", `${marginLeft}px`)
-    .style("width", `${boardSize}px`)
-    .style("height", `${boardSize}px`)
-    .style("border", `10px solid ${whiteSquareColour}`)
+  .select('body')
+  .append('div')
+  .style('position', 'fixed')
+  .style('top', `${marginTop}px`)
+  .style('left', `${marginLeft}px`)
+  .style('width', `${boardSize}px`)
+  .style('height', `${boardSize}px`)
+  .style('border', `10px solid ${whiteSquareColour}`)
 
 const svg = div
-    .append("svg")
-    .attr("width", `${boardSize}px`)
-    .attr("height", `${boardSize}px`)
-    .selectAll()
-    .data(board)
-    .enter()
-    .append("g")
+  .append('svg')
+  .attr('width', `${boardSize}px`)
+  .attr('height', `${boardSize}px`)
+  .selectAll()
+  .data(board)
+  .enter()
+  .append('g')
 
 svg
-    .append("rect")
-    .attr("x", d => d.x * squareSize)
-    .attr("y", d => d.y * squareSize)
-    .attr("width", `${squareSize}px`)
-    .attr("height", `${squareSize}px`)
-    .style("fill", d => isWhiteSquare(d.x, d.y) ? whiteSquareColour : blackSquareColour)
-    .on('click', (e, d) => console.log(e.target, d, getSquareName(d)))
+  .append('rect')
+  .attr('x', (d) => d.x * squareSize)
+  .attr('y', (d) => d.y * squareSize)
+  .attr('width', `${squareSize}px`)
+  .attr('height', `${squareSize}px`)
+  .style('fill', (d) => (isWhiteSquare(d.x, d.y) ? whiteSquareColour : blackSquareColour))
+  .on('click', (e, d) => console.log(e.target, d, getSquareName(d)))
 
 svg
-    .append('text')
-    .attr("x", d => d.x * squareSize + squareSize / 2)
-    .attr("y", d => d.y * squareSize + squareSize / 2)
-    .attr("text-anchor", "middle")
-    .attr("dominant-baseline", "middle")    
-    .style('font-family', 'Verdana')
-    .text(d => getSquareName(d))
-
-
-
-
+  .append('text')
+  .attr('x', (d) => d.x * squareSize + squareSize / 2)
+  .attr('y', (d) => d.y * squareSize + squareSize / 2)
+  .attr('text-anchor', 'middle')
+  .attr('dominant-baseline', 'middle')
+  .style('font-family', 'Verdana')
+  .text((d) => getSquareName(d))
